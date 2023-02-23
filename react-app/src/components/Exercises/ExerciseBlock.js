278250cdae32625295edf1a76e7806d6a668ba0d
Christopher Dobson <christopher.anthony.dobson@gmail.com>
2023-02-23 10:55:04 -0500

    implement expanding description on exercises

--- react-app/src/components/Exercises/ExerciseBlock.js
+++ react-app/src/components/Exercises/ExerciseBlock.js
@@ -1,17 +1,25 @@
+import { useState } from "react";
 import defaultImg from "./no_preview_img.png"
+import { Link } from "react-router-dom";
 
 const ExerciseBlock = ({ exercise }) => {
   const { creator_id, description, id, motion_img_url, name} = exercise
-  const truncatedDescription = description.length > 500 ? description.substring(0, 497) + "..." : description;
+  const [showFullDescription, setShowFullDescription] = useState(false)
+
+
+  const truncatedDescription = description.length > 400 ? description.substring(0, 397) + "...(click to show more)" : description;
   const imgUrl = motion_img_url ? motion_img_url : defaultImg
 
   console.log(description)
 
   return (
     <div className="exercise-block">
-        <div className="exercise-block-description">
+        <div className="exercise-block-text">
           <h3>{name}</h3>
-          {truncatedDescription}
+          <Link to={`/exercises/${id}`}>View Exercise Page</Link>
+          <div className="exercise-block-description" onClick={() => {setShowFullDescription(!showFullDescription)}}>
+            {showFullDescription ? description : truncatedDescription}
+          </div>
         </div>
         <div className="exercise-block-preview-img">
           <img src={imgUrl} alt={`preview of exercise: ${name}`} />
