8d29e755ecb33c893065c0301515e427dc0d2aac
Christopher Dobson <christopher.anthony.dobson@gmail.com>
2023-03-02 09:34:48 -0500

    add details to the RE Block

--- react-app/src/components/Routines/RoutineExerciseBlock.js
+++ react-app/src/components/Routines/RoutineExerciseBlock.js
@@ -1,10 +1,16 @@
 
 
 const RoutineExerciseBlock = ({routineExercise}) => {
+  const {order, Exercise, exercise_id, sets_reps_array, instructions} = routineExercise
 
   return (
     <div className="routine-exercise-block">
-      re
+      <h2>Step {order}: {Exercise.name}</h2>
+      <div>Sets/Reps/Rest: {sets_reps_array}</div>
+      {instructions && (
+        <div>Instructions: {instructions}</div>
+      )}
+      
     </div>
   )
 }
