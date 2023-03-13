1b4393610b4e1f1e386a6a173bb35f81b995330b
christopher <christopher.anthony.dobson@gmail.com>
2023-03-13 15:42:27 -0400

    add basic homepage and favicon

--- react-app/src/components/Homepage/Homepage.js
+++ react-app/src/components/Homepage/Homepage.js
@@ -1,9 +1,15 @@
-
+import Link from 'react-router-dom/Link'
 
 const Homepage = () => {
   return (
     <>
-      <h1>Homepage</h1>
+      <h1>Rep Recorder</h1>
+      <div>
+        <h2>What is Rep Recorder?</h2>
+        <p>Rep Recorder is a web application that allows you to track your exercise performance for your favorite exercises.</p>
+        <p>To get started, click on <Link to="/exercises">exercises</Link> to explore exercises to add to your routine, or click on <Link to="/routines">routines</Link> to view and build a routine.</p>
+
+      </div>
     </>
   )
 }
